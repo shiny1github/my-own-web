0a030faf6b8bf53520700e54fe673289226f1417
shiavni <shivanisuman298@gmail.com>
2021-12-05 23:22:50 +0530

    create about ui

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from "react";
+import mypic from "../images/myweb.jpg";
 
 const About = () => {
-    return (
-        <div>
-            <div>
-            <p >WELCOME</p>
-            <h1>about us</h1>
-            
-        </div>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="container emp-profile">
+        <form method="">
+          <div className="row">
+            <div className="col-md-2">
+              <img src={mypic} alt="mypic" />
+            </div>
 
-export default About
+            <div className="col-md-7">
+              <div className="profile-head">
+                <h4>Shivani Suman</h4>
+                <h6>Web Developer</h6>
+                <br></br>
+                <p>
+                  “A successful website does three things: <br></br> It attracts the right
+                  kinds of visitors. <br></br>Guides them to the main services or product
+                  you offer. <br></br>Collect Contact details for future ongoing
+                  relation.”
+                </p>
+              </div>
+            </div>
+            <div className="col-md-2">
+              <input
+                type="text"
+                className="profile-edit-btn"
+                value="Edit Profile"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default About;
